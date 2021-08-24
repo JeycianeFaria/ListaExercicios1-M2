949b45517c37baafe786446639c75586645a53f0
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-24 20:41:32 -0300

    Adicionando programa que imprime a mensagem 'Alô mundo'

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        //Imprimir na tela a mensagem "Alô mundo"
+        System.out.print("Alô mundo!");
+
+    }
+
+}
