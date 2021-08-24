6932ba8f6ee1c1e40c19c2b0dff013c67213c07e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-24 20:53:40 -0300

    Adicionando variáveis para armazenar as notas

---
+++ src/br/com/zup/Exercicio2.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        //criando as variaveis para as notas
+        float nota1 = 7F;
+        float nota2 = 6F;
+        float nota3 = 8.5F;
+        float nota4 = 9F;
+
+    }
+
+}
