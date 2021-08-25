2e9d000f1233278c26f6c6e0b70e393aa3fa7f49
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-24 22:15:56 -0300

    Criando variavel para calculo do salário líquido e imprimindo os resultados

--- src/br/com/zup/Exercicio6.java
+++ src/br/com/zup/Exercicio6.java
@@ -14,6 +14,16 @@ public class Exercicio6 {
         float valorInss = salarioBruto * (8/100F);
         float valorSindicato = salarioBruto * (5/100F);
 
+        //Calculando o salário líquido
+        float salarioLiquido = salarioBruto - valorImpostoDeRenda - valorInss - valorSindicato;
+
+
+        //imprimindo valores
+        System.out.println("O seu sálario bruto é de R$ " + salarioBruto);
+        System.out.println("Foi descontado para o INSS o total de R$ " + valorInss);
+        System.out.println("Foi descontado para o Sindicato o total de R$ " + valorSindicato);
+        System.out.println("Foi descontado para o Imposto de Renda  o total de R$ " + valorImpostoDeRenda);
+        System.out.println("Então o seu salário líquido é de R$ " + salarioLiquido);
 
     }
 
