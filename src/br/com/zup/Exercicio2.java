56c0ca78e9c381d0ed27b5f8fa01a1dd49602cc9
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-24 21:05:47 -0300

    Imprimindo a média aritmética  para o usuário

--- src/br/com/zup/Exercicio2.java
+++ src/br/com/zup/Exercicio2.java
@@ -16,6 +16,10 @@ public class Exercicio2 {
         //criando variavel para calcular a média aritmética
         float mediaAritmetica = somaDasNotas/4;
 
+        //imprimindo a média aritmética  para o usuário
+        System.out.println("Suas notas do bimestre foram as seguintes: " + nota1 +  ", " + nota2 + ", " + nota3 + " e " + nota4 );
+        System.out.println("E sua média aritmética é " + mediaAritmetica);
+
     }
 
 }
