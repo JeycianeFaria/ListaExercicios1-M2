d33ffba2373ad11ccb500998abaadb87d4a78e58
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-24 21:19:21 -0300

    Criando programa que converte metros em centimetros

--- src/br/com/zup/Exercicio2.java
+++ src/br/com/zup/Exercicio2.java
@@ -10,11 +10,14 @@ public class Exercicio2 {
         float nota3 = 8.5F;
         float nota4 = 9F;
 
+
         //criando a variavel para realizar a soma das notas
         float somaDasNotas = nota1 + nota2 + nota3 +nota4;
 
+
         //criando variavel para calcular a média aritmética
         float mediaAritmetica = somaDasNotas/4;
+
 
         //imprimindo a média aritmética  para o usuário
         System.out.println("Suas notas do bimestre foram as seguintes: " + nota1 +  ", " + nota2 + ", " + nota3 + " e " + nota4 );
